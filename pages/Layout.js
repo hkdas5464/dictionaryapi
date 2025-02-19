39dac17c65e11853aa8240753d22bc755c5d1735
Harendra Kumar Das <63789292+hkdas5464@users.noreply.github.com>
2025-02-19 19:49:17 +0530

    Update Layout.js

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,6 +1,6 @@
 // app/layout.js
 import ThemeProvider from '../components/ThemeProvider';
-import { Spotlight } from '@/components/ui /Spotlight';
+import { Spotlight } from '@/components/ui/Spotlight';
 export const metadata = {
   title: 'Your App Title',
   description: 'Your App Description',

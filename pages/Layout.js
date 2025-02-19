bc964cd47b4884f500747ad844e2ab760c779ef0
Harendra Kumar Das <63789292+hkdas5464@users.noreply.github.com>
2025-02-19 19:46:47 +0530

    Update Layout.js

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,6 +1,6 @@
 // app/layout.js
 import ThemeProvider from '../components/ThemeProvider';
-
+import { Spotlight } from '@/components/ui /Spotlight';
 export const metadata = {
   title: 'Your App Title',
   description: 'Your App Description',
@@ -9,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+     <body>
+<Spotlight fill="blue"/>
+ 
         <ThemeProvider>
           {children}
         </ThemeProvider>
